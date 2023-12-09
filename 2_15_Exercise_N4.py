08a5ca618a429ab9ee72b0c0a465cf2828c89fc4
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-09 21:01:41 +0200

    Add files via upload

---
+++ 2_15_Exercise_N4.py
@@ -0,0 +1,11 @@
+width = 17
+height = 12.0
+
+# За всеки от следните изрази
+# напишете стойността на израза
+# и вида (на стойността на израза).
+
+print(width//2, type(width//2))
+print(width/2.0, type(width/2.0))
+print(height/3, type(height/3))
+print(1 + 2 * 5, type(1 + 2 * 5))
