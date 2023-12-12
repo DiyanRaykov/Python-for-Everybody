6b8f1629538a539d7d4cec595c4585788b9d73a3
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-12 18:22:20 +0200

    Add files via upload

---
+++ 3_11_Exercises_N1.py
@@ -0,0 +1,14 @@
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+# over_pay = 0
+hrs = float(hrs)
+rate = float(rate)
+
+if float(hrs) > 40:
+    extra_hrs = hrs - 40
+    over_pay = extra_hrs * rate * 1.5
+    pay = (hrs - extra_hrs) * rate + over_pay
+else:
+    pay = hrs * rate
+
+print(f"{pay:.2f}")
