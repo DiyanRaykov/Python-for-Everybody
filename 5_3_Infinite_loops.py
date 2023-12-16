fb0577cfc0e205a183134ec266c1869946af7d26
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-16 14:02:55 +0200

    Add files via upload

---
+++ 5_3_Infinite_loops.py
@@ -0,0 +1,12 @@
+# n = 10
+# while True:
+#     print(n, end=' ')
+#     n = n - 1
+# print('Done!')
+
+while True:
+    line = input('> ')
+    if line == 'done':
+        break
+    print(line)
+print('Done!')
