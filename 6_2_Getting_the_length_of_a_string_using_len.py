2b2a33da6289e24b7c667ddbf3e508f639a753ac
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-19 19:14:41 +0200

    Add files via upload

---
+++ 6_2_Getting_the_length_of_a_string_using_len.py
@@ -0,0 +1,12 @@
+# fruit = 'banana'
+# len(fruit)
+# print(len(fruit))
+
+fruit = 'banana'
+length = len(fruit)
+last = fruit[length-1]
+print(last)
+print(fruit[-1])
+print(fruit[-2])
+
+# fruit = 'banana'
