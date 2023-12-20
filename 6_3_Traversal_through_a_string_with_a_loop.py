e589ca866bf743459e6e8597d7dc16d4f1e441d4
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-20 21:30:09 +0200

    Add files via upload

---
+++ 6_3_Traversal_through_a_string_with_a_loop.py
@@ -0,0 +1,10 @@
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter, end=" ")
+    index = index + 1
+
+# fruit = 'banana'
+# for char in fruit:
+#     print(char)
