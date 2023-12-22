bd2e1dfcda8cc5f0605468d9372ae7011527d2ed
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-22 18:37:17 +0200

    Add files via upload

---
+++ 6_8_String_comparison.py
@@ -0,0 +1,10 @@
+word = input('Enter your word: ')
+fruit = 'banana'
+if word == 'banana':
+    print('All right, bananas.')
+if word < 'banana':
+    print('Your word,' + word + ', comes before banana.')
+elif word > 'banana':
+    print('Your word,' + word + ', comes after banana.')
+else:
+    print('All right, bananas.')
