c096f70f5ae8639af93c68722b47332c9fb334d3
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-23 18:23:21 +0200

    Add files via upload

---
+++ 6_9_String_methods.py
@@ -0,0 +1,12 @@
+# stuff = 'Hello world'
+# print(type(stuff))
+#
+# print(dir(stuff))
+
+line = '   Have a nice day   '
+line.strip()
+print(line.strip())
+
+line.startswith('h')
+line.lower()
+line.lower().startswith('h')
