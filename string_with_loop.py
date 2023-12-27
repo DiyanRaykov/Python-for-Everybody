697112fcf002ac2ecab4458e3cce53600517c383
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2023-12-27 17:36:19 +0200

    Add files via upload

---
+++ string_with_loop.py
@@ -0,0 +1,11 @@
+fruit = 'banana'
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print(index, letter)
+    index = index + 1
+
+# The same result print next code:
+# fruit = 'banana'
+# for letter in fruit:
+#     print(letter)
