f45af82a25f7a70bb55ad142766c881c44e43c46
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-01-08 17:50:02 +0200

    Add files via upload

---
+++ 8_3_Traversing_a_list.py
@@ -0,0 +1,8 @@
+cheeses = ['Cheddar', 'Edam', 'Gouda']
+for cheese in cheeses:
+    print(cheese)
+
+# numbers = [17, 123]
+# for i in range(len(numbers)):
+#     numbers[i] = numbers[i] * 2
+#     print(numbers)
