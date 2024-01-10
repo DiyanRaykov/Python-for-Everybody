a0cf94921a75cc3d58364e77bdc3e1400bbc02b3
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-01-10 13:12:54 +0200

    Add files via upload

---
+++ 8_7_Deleting_elements.py
@@ -0,0 +1,15 @@
+# t = ['a', 'b', 'c']
+# x = t.pop(1)
+# print(t)
+
+# t = ['a', 'b', 'c']
+# del t[1]
+# print(t)
+
+# t = ['a', 'b', 'c']
+# t.remove('b')
+# print(t)
+
+t = ['a', 'b', 'c', 'd', 'e', 'f']
+del t[1:5]
+print(t)
