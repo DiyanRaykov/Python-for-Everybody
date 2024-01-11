afc8c28ff096182e0d647071cd100bfdbabbd6de
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-01-11 19:56:13 +0200

    Add files via upload

---
+++ 8_9_Lists_and_strings.py
@@ -0,0 +1,23 @@
+# s = 'spam'
+# t = list(s)
+# print(t)
+
+# s = 'pining for the fjords'
+# t = s.split()
+# print(t[2])
+
+# s = 'spam-spam-spam'
+# delimiter = '-'
+# s.split(delimiter)
+# print(s.split(delimiter))
+
+t = ['pining', 'for', 'the', 'fjords']
+delimiter = ' '
+delimiter.join(t)
+print(delimiter.join(t))
+
+
+
+
+
+
