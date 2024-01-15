9cd8ab752c378254edceb935d69716037ff223d6
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-01-15 21:43:48 +0200

    Add files via upload

---
+++ 8_13_List_arguments.py
@@ -0,0 +1,19 @@
+# def delete_head(t):
+#     del t[0]
+#
+# letters = ['a', 'b', 'c']
+# delete_head(letters)
+# print(letters)
+
+# t1 = [1, 2]
+# t2 = t1.append(3)
+# print(t1)
+#
+# print(t2)
+# #
+# def tail(t):
+#     return t[1:]
+#
+# letters = ['a', 'b', 'c']
+# rest = tail(letters)
+# print(rest)
