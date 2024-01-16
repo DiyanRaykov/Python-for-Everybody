7452c5a19ae772b4ceb4de920521158ac551d243
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-01-16 20:16:37 +0200

    Add files via upload

---
+++ 8_16_Exercises_N2.py
@@ -0,0 +1,16 @@
+file_name = input('Enter the file name: ') # mbox-short.txt
+try:
+    fhand = open(file_name)
+except:
+    print('The file name is not correct!', file_name)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    # print('Debug:', words)
+    if len(words) == 0 : continue
+    if words[0] != 'From' : continue
+
+    print(words[2])
+
+
