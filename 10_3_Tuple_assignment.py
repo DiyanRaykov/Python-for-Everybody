12f488713eb17b6aae2562822792e1e87d7ebfe3
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-01-25 20:13:31 +0200

    Add files via upload

---
+++ 10_3_Tuple_assignment.py
@@ -0,0 +1,11 @@
+# x, y, z = [ 'have', 'fun', 'yes' ]
+#
+# print(x, y, z)
+
+# a, b, c = 1, 2, 3
+# print(a,b,c)
+
+addr = 'monty@python.org'
+uname, domain = addr.split('@')
+print(uname)
+print(domain)
