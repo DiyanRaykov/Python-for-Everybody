88a519f79706c38f354ba99d948775ae813907c9
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-02-10 20:13:52 +0200

    Add files via upload

---
+++ 12_7_Parsing_HTML_using_regular_expressions.py
@@ -0,0 +1,14 @@
+import urllib.request, urllib.parse, urllib.error
+import re
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ') #https://docs.python.org
+html = urllib.request.urlopen(url, context=ctx).read()
+links = re.findall(b'href="(http[s]?://.*?)"', html)
+for link in links:
+    print(link.decode())
