582e6fd1f14550802818578381cee0cbfc2a251a
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-02-16 21:19:45 +0200

    Add files via upload

---
+++ 12_11_1_Exam_Exercises.py
@@ -0,0 +1,35 @@
+import socket
+import re
+
+url = input('Enter a URL address: ')    # http://data.pr4e.org/intro-short.txt
+if len(url) < 1: url = 'http://data.pr4e.org/intro-short.txt'
+url1 = url.split('/')
+host = url1[2]
+
+# Validate format URL
+url_pattern = "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+if (not re.search(url_pattern, url)):
+    print('The URL is not formatted!', url)
+    exit()
+
+# Validate open URL
+try:
+    mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mysock.connect((host, 80))
+except:
+    print('The URL is not correct!', url)
+    exit()
+
+cmd = f'GET {url} HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    # Validate exist URL
+    if data.decode().find('404 Not Found') == -1:
+        print(data.decode(), end='')
+    else:
+        print('The URL is not found!', url)
+
+mysock.close()
