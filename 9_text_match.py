04d38366e6cdc980c4a95760ae28a672dcb1ee82
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-03-07 20:12:43 +0200

    Add files via upload

---
+++ 9_text_match.py
@@ -0,0 +1,15 @@
+# Write a Python program that
+# matches a string that has an
+# 'a' followed by anything ending in 'b'.
+
+import re
+def text_match(text):
+        patterns = 'a.*?b$'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+
+print(text_match("aabbbbd"))
+print(text_match("aabAbbbc"))
+print(text_match("accddbbjjjb"))
