04931c3c6bc70d834c2f217fbf7c6d7a99e495ab
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-03-12 20:01:02 +0200

    Add files via upload

---
+++ 15_text_match.py
@@ -0,0 +1,13 @@
+# Write a Python program that
+# starts each string with
+# a specific number.
+
+import re
+def match_num(string):
+    text = re.compile(r"^5")
+    if text.match(string):
+        return True
+    else:
+        return False
+print(match_num('5-2345861'))
+print(match_num('6-2345861'))
