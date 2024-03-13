f7f6a6e4125a967048bcbd216cb93ca9c6d03af1
Diyan Todorov <146946767+DiyanRaykov@users.noreply.github.com>
2024-03-13 17:18:18 +0200

    Add files via upload

---
+++ 17_text_match.py
@@ -0,0 +1,13 @@
+# Write a Python program to check
+# for a number at the end of a string.
+
+import re
+def end_num(string):
+    text = re.compile(r".*[0-9]$")
+    if text.match(string):
+        return True
+    else:
+        return False
+
+print(end_num('abcdef'))
+print(end_num('abcdef6'))
